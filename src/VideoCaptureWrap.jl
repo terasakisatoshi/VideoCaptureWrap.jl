9229227fafd9c89ecf31a2e0c5f54ab078105ca5
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-10-28 15:10:57 +0900

    Add code for build source from scratch

--- src/VideoCaptureWrap.jl
+++ src/VideoCaptureWrap.jl
@@ -3,6 +3,13 @@ module VideoCaptureWrap
 using CxxWrap
 using VideoCaptureWrap_jll
 const libvideocapture = VideoCaptureWrap_jll.libvideocapture_path
+
+#= 
+# For those who like to use your shared library
+using Libdl
+const libvideocapture = joinpath("build", "lib", "libvideocapture.$(Libdl.dlext)")
+=#
+
 isfile(libvideocapture) && @wrapmodule(libvideocapture, :define_videoio_module)
 
 function __init__()
